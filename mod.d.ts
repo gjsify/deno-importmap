c6ab08623daf068ec24d8ea76451be490cbf8511
dalcib <dalcib@gmail.com>
2021-01-16 01:18:24 +0530

    version 0.1.5

--- mod.d.ts
+++ mod.d.ts
@@ -1,23 +1,27 @@
-declare module "mod" {
-    interface SpecifierMap {
-        [specifier: string]: string;
-    }
-    interface Scopes {
-        [url: string]: SpecifierMap;
-    }
-    export interface ImportMap {
-        imports?: SpecifierMap;
-        scopes?: Scopes;
-    }
-    /**
-     * resolves specifier with import map.
-     * ```ts
-     * import { resolve } from "deno-importmap"
-     *
-     * const specifier = "foo/mod.ts"
-     * const importMap = { imports: { "foo/": "bar/" } }
-     * const resolvedSpecifier = resolve(specifier, importMap) // returns "bar/mod.ts"
-     * ```
-     */
-    export function resolve(specifier: string, importMap: ImportMap, baseURL?: string): string;
+declare module 'deno-importmap' {
+  export interface SpecifierMap {
+    [specifier: string]: string
+  }
+  export interface Scopes {
+    [url: string]: SpecifierMap
+  }
+  export interface ImportMap {
+    imports?: SpecifierMap
+    scopes?: Scopes
+  }
+  /**
+   * resolves specifier with import map.
+   * ```ts
+   * import { resolve } from "https://deno.land/x/importmap/mod.ts"
+   *
+   * const specifier = "foo/mod.ts"
+   * const importMap = { imports: { "foo/": "bar/" } }
+   * const resolvedSpecifier = resolve(specifier, importMap) // returns "bar/mod.ts"
+   * ```
+   */
+  export function resolve(
+    specifier: string,
+    { imports, scopes }: ImportMap,
+    baseURL?: string
+  ): string
 }
